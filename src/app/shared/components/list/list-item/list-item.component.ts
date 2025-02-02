c733096064ff5f5d59280838ad5909a5606f1ba0
Aico Klein Ovink <aicokleinovink@gmail.com>
2025-02-02 22:12:09 +0100

    Display icons for list items

--- src/app/shared/components/list/list-item/list-item.component.ts
+++ src/app/shared/components/list/list-item/list-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faForward, faPlus, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'sr-list-item',
@@ -7,5 +9,19 @@ import { Component, input, output } from '@angular/core';
 export class ListItemComponent {
   public readonly label = input.required<string>();
   public readonly routerLink = input<string | (string | number)[]>();
+  public readonly iconType = input<'link' | 'add' | 'delete'>('link');
   public readonly select = output<void>();
+  public readonly icon = computed(() => this.getIcon());
+
+  private getIcon(): IconDefinition {
+    switch (this.iconType()) {
+      default:
+      case 'link':
+        return faForward;
+      case 'add':
+        return faPlus;
+      case 'delete':
+        return faXmark;
+    }
+  }
 }
--- src/app/shared/components/list/list.component.ts
+++ src/app/shared/components/list/list.component.ts
@@ -1,14 +1,14 @@
-import { Component, contentChildren } from '@angular/core';
-import { ListItemComponent } from './list-item/list-item.component';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Component, contentChildren } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @Component({
   selector: 'sr-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
-
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FontAwesomeModule],
 })
 export class ListComponent {
   protected readonly listItems = contentChildren(ListItemComponent);
