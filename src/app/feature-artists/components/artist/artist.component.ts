e8a3ef6db2feaed46bea84373c136e5edef6602b
Aico Klein Ovink <aicokleinovink@gmail.com>
2025-02-04 12:28:40 +0100

    Simplified and refactor getting spotify albums

--- src/app/feature-artists/components/artist/artist.component.ts
+++ src/app/feature-artists/components/artist/artist.component.ts
@@ -1,7 +1,7 @@
-import { Component, computed, inject, input, resource } from '@angular/core';
+import { Component, inject, input, resource } from '@angular/core';
 import { patchState, signalState } from '@ngrx/signals';
 import { AlbumComponent, AlertComponent, ListModule, PlaylistWidgetComponent } from '../../../shared/components';
-import { Album, Playlist, Song } from '../../../shared/models';
+import { Playlist, Song } from '../../../shared/models';
 import { SpotifyService } from '../../../shared/services';
 import { PlaylistsStore } from '../../../shared/store';
 import { SongsService } from '../../services/songs.service';
@@ -20,19 +20,17 @@ interface ComponentState {
 export class ArtistComponent {
   private readonly songsService = inject(SongsService);
   private readonly spotifyService = inject(SpotifyService);
-
-  public readonly artistName = input.required<string>();
   protected readonly playlistsStore = inject(PlaylistsStore);
-  protected readonly albumsByArtist = computed(() => this.getAlbumsByArtist());
+  public readonly artistName = input.required<string>();
 
   protected readonly songsResource = resource({
     request: () => ({ artistName: this.artistName() }),
-    loader: ({ request }) => this.getSongsByArtist(request.artistName),
+    loader: ({ request }) => this.songsService.getSongsByArtist(request.artistName),
   });
 
-  protected readonly tracksResource = resource({
-    request: () => ({ songs: this.songsResource.value() }),
-    loader: ({ request }) => this.spotifyService.getTracks(request.songs ?? []),
+  protected readonly albumsResource = resource({
+    request: () => ({ songs: this.songsResource.value() ?? [] }),
+    loader: ({ request }) => this.spotifyService.getAlbumnsBySongs(request.songs),
   });
 
   protected readonly state = signalState<ComponentState>({
@@ -70,20 +68,5 @@ export class ArtistComponent {
     }
 
     this.closePlaylistWidget();
-  }
-
-  private getSongsByArtist(artist: string): Promise<Song[]> {
-    return this.songsService.getSongsByArtist(artist);
-  }
-
-  private getAlbumsByArtist(): Album[] {
-    return (this.tracksResource.value()?.tracks ?? []).reduce<Album[]>((albums, track) => {
-      const isDuplicate = albums.some(({ name }) => name === track.album.name);
-      const hasImages = track.album.images.length > 0;
-
-      return isDuplicate || !hasImages
-        ? albums
-        : [...albums, { name: track.album.name, imageUrl: track.album.images[0].url }];
-    }, []);
   }
 }
--- src/app/shared/services/spotify/spotify.service.ts
+++ src/app/shared/services/spotify/spotify.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { firstValueFrom, of } from 'rxjs';
+import { firstValueFrom, map, of } from 'rxjs';
+import { Album, Song } from '../../models';
 import { SpotifyAccessTokenResponse, SpotifyTracksResponse } from './models';
-import { Song } from '../../models';
 
 @Injectable({ providedIn: 'root' })
 export class SpotifyService {
@@ -11,30 +11,39 @@ export class SpotifyService {
 
   public async getAccessToken(): Promise<void> {
     const tokenUrl = 'https://accounts.spotify.com/api/token';
+    const headers = new HttpHeaders().append('Content-Type', 'application/x-www-form-urlencoded');
     const body = new URLSearchParams({
       grant_type: 'client_credentials',
       client_id: '5b92e62682df4fc498c07a14f9a53444',
       client_secret: '606d061c384745e88ea83dcc7df3c422',
     });
 
-    const response = await firstValueFrom(
-      this.http.post<SpotifyAccessTokenResponse>(tokenUrl, body, {
-        headers: new HttpHeaders().append('Content-Type', 'application/x-www-form-urlencoded'),
-      })
-    );
-
+    const response = await firstValueFrom(this.http.post<SpotifyAccessTokenResponse>(tokenUrl, body, { headers }));
     this.accessToken = response.access_token;
   }
 
-  public getTracks(songs: Song[]): Promise<SpotifyTracksResponse | null> {
+  public getAlbumnsBySongs(songs: Song[]): Promise<Album[]> {
     const tracksUrl = 'https://api.spotify.com/v1/tracks';
     const trackIds = songs.map(({ spotifyId }) => spotifyId);
     const params = { ids: trackIds.join(',') };
     const headers = this.getAuthorizationHeader();
 
     return firstValueFrom(
-      params.ids.length ? this.http.get<SpotifyTracksResponse>(tracksUrl, { params, headers }) : of(null)
+      params.ids.length
+        ? this.http.get<SpotifyTracksResponse>(tracksUrl, { params, headers }).pipe(map((data) => this.getAlbums(data)))
+        : of([])
     );
+  }
+
+  private getAlbums(data: SpotifyTracksResponse | null): Album[] {
+    return (data?.tracks ?? []).reduce<Album[]>((albums, track) => {
+      const isDuplicate = albums.some(({ name }) => name === track.album.name);
+      const hasImages = track.album.images.length > 0;
+
+      return isDuplicate || !hasImages
+        ? albums
+        : [...albums, { name: track.album.name, imageUrl: track.album.images[0].url }];
+    }, []);
   }
 
   private getAuthorizationHeader(): HttpHeaders {
