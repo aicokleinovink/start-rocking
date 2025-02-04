782fa52b68a64612e19e48b9c735b0293c0453be
Aico Klein Ovink <aicokleinovink@gmail.com>
2025-02-04 15:55:34 +0100

    Typo

--- e2e/src/app.spec.ts
+++ e2e/src/app.spec.ts
@@ -2,19 +2,19 @@ import { expect, test } from '@playwright/test';
 import { TopbarPO } from './page-objects';
 
 test.describe('App', () => {
-  let headerPO: TopbarPO;
+  let topbarPO: TopbarPO;
 
   test.beforeEach(async ({ page }) => {
-    headerPO = new TopbarPO(page);
+    topbarPO = new TopbarPO(page);
     await page.goto('/');
   });
 
   test('should display application header', async ({ page }) => {
-    await expect(headerPO.applicationName).toBeVisible();
-    await expect(headerPO.artistsLink).toBeVisible();
-    await expect(headerPO.playlistsLink).toBeVisible();
-    await expect(headerPO.artistsLink).toHaveClass('active');
-    await expect(headerPO.playlistsLink).not.toHaveClass('active');
+    await expect(topbarPO.applicationName).toBeVisible();
+    await expect(topbarPO.artistsLink).toBeVisible();
+    await expect(topbarPO.playlistsLink).toBeVisible();
+    await expect(topbarPO.artistsLink).toHaveClass('active');
+    await expect(topbarPO.playlistsLink).not.toHaveClass('active');
     expect(page.url()).toContain('/artiesten');
   });
 });
